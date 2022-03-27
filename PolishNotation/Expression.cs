18ff733ea95bbdeaf87f136ebffef29f531c6a80
stlad <82213506+stlad@users.noreply.github.com>
2022-03-27 19:17:06 +0500

    поддержка некоторых унарных функций

--- PolishNotation/Expression.cs
+++ PolishNotation/Expression.cs
@@ -10,19 +10,23 @@ namespace PolishNotation
     {
         public string InFix { get; private set; }
 
-        public string PostFix { get; private set; }
+        public List<string> PostFix { get; private set; }
+
 
         public double Result { get; private set; }
 
-        private static Dictionary<char, int> Priority = new Dictionary<char, int>
+        private static Dictionary<string, int> Priority = new Dictionary<string, int>
         {
-            { '(', 0 },
-            { '+', 1 },
-            { '-', 1 },
-            { '*', 2 },
-            { '/', 2 },
-            { '^', 3 },
-            { '~', 4 } //унарный минус
+            { "(", 0 },
+            { "+", 1 },
+            { "-", 1 },
+            { "*", 2 },
+            { "/", 2 },
+            { "^", 3 },
+            { "~", 4 }, //унарный минус
+            { "sin", 4 },
+            { "cos", 4 },
+            { "sqrt",4 }
         };
         public Expression(string infFixExpr)
         {
@@ -30,41 +34,43 @@ namespace PolishNotation
             PostFix = InFixToPostFix(InFix);
         }
 
-        private static string InFixToPostFix(string infExpr)
-        {
-            var res = "";
-            var stack = new Stack<char>();
 
+        private static List<string> InFixToPostFix(string infExpr)
+        {
+            var res = new List<string>();
+            var stack = new Stack<string>();
             for(int i=0; i<infExpr.Length; i++)
             {
                 var currentChar = infExpr[i];
 
                 if (Char.IsDigit(currentChar))
-                    res += getNumberFromStr(infExpr, ref i) + " ";
+                    res.Add(getNumberFromStr(infExpr, ref i));
+                else if (Char.IsLetter(currentChar))
+                    stack.Push(getFuncFromStr(infExpr, ref i));
                 else if (currentChar == '(')
-                    stack.Push(currentChar);
+                    stack.Push(Convert.ToString(currentChar));
                 else if (currentChar == ')')
                 {
-                    while (stack.Count > 0 && stack.Peek() != '(')
-                        res += stack.Pop();
+                    while (stack.Count > 0 && stack.Peek() != "(")
+                        res.Add(stack.Pop());
                     stack.Pop();
                 }
-                else if(Priority.ContainsKey(currentChar))
+                else if (Priority.ContainsKey(Convert.ToString( currentChar)))
                 {
-                    char op = currentChar;
-                    if (op == '-' && (i == 0 || (i > 1 && Priority.ContainsKey(infExpr[i - 1]))))
-                        op = '~';
+                    var op = Convert.ToString( currentChar);
+                    if (op == "-" && (i == 0 || (i > 1 && Priority.ContainsKey(Convert.ToString( infExpr[i-1])))))
+                        op = "~";
 
 
                     while (stack.Count > 0 && (Priority[stack.Peek()] >= Priority[op]))
-                        res += stack.Pop();
+                        res.Add(stack.Pop());
 
                     stack.Push(op);
                 }
             }
 
             foreach (var op in stack)
-                res += op;
+                res.Add(op);
             return res;
         }
 
@@ -90,7 +96,7 @@ namespace PolishNotation
             return res;
         }
 
-        public static string getFuncFromStr(string str, ref int index)
+        private static string getFuncFromStr(string str, ref int index)
         {
             var res = "";
 
@@ -112,18 +118,24 @@ namespace PolishNotation
             return res;
         }
 
-        private static double ExecuteOperator(double a, double b, char op)
+        private static double ExecuteOperator(double a, double b, string op)
         {
             double res = 0;
             switch (op)
             {
-                case '+': return a + b;
-                case '-': return a - b;
-                case '*': return a * b;
-                case '/':
+                case "+": return a + b;
+                case "-": return a - b;
+                case "*": return a * b;
+                case "/":
                     if (b == 0) throw new DivideByZeroException();
                     return a / b;
-                case '^': return Math.Pow(a,b);
+                case "^": return Math.Pow(a,b);
+                case "sin": 
+                    return Math.Sin(a);
+                case "cos":
+                    return Math.Cos(a);
+                case "sqrt":
+                    return Math.Sqrt(a);
                 default: return 0;
             }
         }
@@ -132,33 +144,51 @@ namespace PolishNotation
         {
             var stack = new Stack<double>();
             int counter = 0;
-            for(int i=0; i< PostFix.Length; i++)
+            for (int i = 0; i < PostFix.Count; i++)
             {
-                if (Char.IsDigit(PostFix[i]))
-                    stack.Push(Convert.ToDouble(getNumberFromStr(PostFix, ref i)));
+                if (Char.IsDigit(PostFix[i][0]))
+                    stack.Push(Convert.ToDouble(PostFix[i]));
                 else if(Priority.ContainsKey(PostFix[i]))
                 {
                     counter++;
-
-                    if(PostFix[i] =='~')
+                    if (PostFix[i] == "~")
                     {
-                        var last = stack.Count>0 ? stack.Pop() : 0;
-                        stack.Push(ExecuteOperator(0, last, '-'));
+                        var last = stack.Count > 0 ? stack.Pop() : 0;
+                        stack.Push(ExecuteOperator(0, last, "-"));
                         continue;
                     }
 
+                    if(PostFix[i] == "sin")
+                    {
+                        var last = stack.Count > 0 ? stack.Pop() : 0;
+                        stack.Push(ExecuteOperator(last, 0, "sin"));
+                        continue;
+                    }
 
+                    if (PostFix[i] == "cos")
+                    {
+                        var last = stack.Count > 0 ? stack.Pop() : 0;
+                        stack.Push(ExecuteOperator(last, 0, "cos"));
+                        continue;
+                    }
+
+                    if (PostFix[i] == "sqrt")
+                    {
+                        var last = stack.Count > 0 ? stack.Pop() : 0;
+                        stack.Push(ExecuteOperator(last, 0, "sqrt"));
+                        continue;
+                    }
 
                     var second = stack.Count > 0 ? stack.Pop() : 0;
                     var first = stack.Count > 0 ? stack.Pop() : 0;
 
-
-                    stack.Push(ExecuteOperator(first, second, PostFix[i]));
+                    stack.Push(ExecuteOperator(first, second, Convert.ToString(PostFix[i])));
                 }
-
             }
 
+
             return stack.Pop();
+            
         }
 
 
