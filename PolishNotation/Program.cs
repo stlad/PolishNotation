49ac977ba4919f367cfac5c2e57ccc3d18390a4a
stlad <82213506+stlad@users.noreply.github.com>
2022-03-27 12:13:32 +0500

    upd

---
+++ PolishNotation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PolishNotation
+{
+    public class Program
+    {
+
+
+        public static void Main()
+        {
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void MainCycle()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Что делаем?");
+                Console.WriteLine("[1]  \n[2]  \n \n\n\n\n[x] Выход");
+                var c = Console.ReadLine();
+                if (c[0] == 'x') break;
+                switch (c[0])
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.Clear();
+                        break;
+                    case '2':
+                        Console.Clear();
+                        Console.Clear();
+                        break;
+                    case 'i':
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("НЕВЕРНАЯ КОММАНДА!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
+            }
+        }
+    }
+}
